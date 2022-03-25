d436148c3e411e485bfc4bd0b53bd470648db623
Felipe Leguizamo <leguizamofelipe@outlook.com>
2022-03-25 00:35:15 -0500

    Demo ddpg file

--- _sawyer_demo_ddpg.py
+++ _sawyer_demo_ddpg.py
@@ -8,8 +8,9 @@ target_dict = {0: Point(0.602,0.681,0.317)}
 
 # for learning_rate in np.array([0.001, 0.005, 0.006, 0.007, 0.008, 0.009, 0.01]):
 env = ContinuousArmMotionEnvironment(target_dict=target_dict)
+# env = gym.make('Pendulum-v0')
 
-time_steps = 10000
+time_steps = 100000
 
 n_actions = env.action_space.shape[-1]
 action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
@@ -17,7 +18,5 @@ action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n
 model = DDPG('MlpPolicy', env, verbose = 1, device = 'cuda:1')
 
 model.learn(total_timesteps=int(time_steps), n_eval_episodes = 30)
-
-env.plot_rewards(added_title = learning_rate)
 
 print('done?')
