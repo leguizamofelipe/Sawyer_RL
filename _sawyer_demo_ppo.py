3f16fa5ba0409e9fac13a5da8e5c66c5784f5189
Felipe Leguizamo <leguizamofelipe@outlook.com>
2022-04-07 17:38:19 -0500

    Fully normalize obs space

--- _sawyer_demo_ppo.py
+++ _sawyer_demo_ppo.py
@@ -2,22 +2,33 @@ from sawyer_continuous_env import *
 from point import Point
 
 from stable_baselines3 import PPO
+
 '''
 target_dict = { 0: Point(0.7315905137589676, 0.7115280197260334, 0.15348379395482878),
                 1: Point(0.7801117441176564, 0.5500329764864156, 0.1842536897312016 ),
                 2: Point(0.685123322687442,  0.6840350631055033, 0.47147110493178634)}
+
+0.623157215	0.377598079	0.330619956
+0.339878021	0.503960918	0.448421076
+0.23093616	0.345445747	0.771748866
+0.525180116	0.634260662	0.365209697
+0.500952516	0.611139185	0.587357806
 '''
 target_dict = { 0: Point(0.602,0.681,0.317)}
 
-# for learning_rate in 1/10*np.array([0.001, 0.005, 0.006, 0.007, 0.008, 0.009, 0.01]):
-env = ContinuousArmMotionEnvironment(target_dict=target_dict)
+# target_dict = { 0: Point(0.385,0.098,0.060)}
 
-time_steps = 10e6
+if len(target_dict) == 1:
+    env_id = f'PPO-{target_dict[0].x}-{target_dict[0].y}-{target_dict[0].z}-{int(time.time())}'
+
+env = ContinuousArmMotionEnvironment(target_dict=target_dict, env_id=env_id)
+
+time_steps = 250000
 
 model = PPO('MlpPolicy', env, verbose = 1, device = 'cuda:1')
 
 model.learn(total_timesteps=int(time_steps), n_eval_episodes = 30)
 
-# env.plot_rewards(added_title = "")
+model.save(os.path.join('logs', env_id, f'model_after{time_steps}'))
 
 print('done?')
--- sawyer_continuous_env.py
+++ sawyer_continuous_env.py
@@ -1,12 +1,19 @@
+from cv2 import normalize
 import gym
 from gym import spaces
 import numpy as np
 import random
+
+from torch import normal
 from sawyer import *
 import pandas as pd
+import os
 
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
+import time
+
+import pickle
 
 from random import randint
 
@@ -16,21 +23,32 @@ class ContinuousArmMotionEnvironment(gym.Env):
     """A robot arm motion environment for OpenAI gym"""
     metadata = {'render.modes': ['human']} # TODO understand what this does
 
-    def __init__(self, save_to_disk=False, target_dict = None):
+    def __init__(self, save_to_disk=False, target_dict = None, env_id = int(time.time())):
         super(ContinuousArmMotionEnvironment, self).__init__()
 
         # Initialize Sawyer
         self.S = Sawyer()
 
+        self.env_id = env_id
+
+        self.save_dir = os.path.join('logs', str(env_id))
+        os.makedirs(self.save_dir)
+        
         # Define which joints are used
         self.active_joints = {   
-                            0 : {'Max': 1.5, 'Min': 0}, 
-                            1 : {'Max': 1, 'Min': -1}, 
-                            #'2', 
-                            3 : {'Max': 2, 'Min': -2}, 
-                            #'4', 
-                            5 : {'Max': 2, 'Min': -2}, 
-                            #'6'
+            0 : {'Max': 1.5, 'Min': 0}, 
+            1 : {'Max': 1, 'Min': -1}, 
+            2 : {'Max': 3, 'Min': -3},  
+            3 : {'Max': 2, 'Min': -2}, 
+            4 : {'Max': 2, 'Min': -2}, 
+            5 : {'Max': 2, 'Min': -2}, 
+            6 : {'Max': 2, 'Min': -2}, 
+        }
+
+        self.box = {
+            'x' : {'Max': 0.2, 'Min': 0.8}, 
+            'y' : {'Max': 0.2, 'Min': 0.8}, 
+            'z' : {'Max': 0.2, 'Min': 0.8},  
         }
 
         self.m_f = 0.01 # Was 0.02
@@ -41,16 +59,15 @@ class ContinuousArmMotionEnvironment(gym.Env):
         self.action_space = spaces.Box(low = -1 * np.ones(len(self.active_joints)), high = np.ones(len(self.active_joints)), dtype=np.float32)
         self.action_space.n = len(self.active_joints)
 
-        # For discrete action space. Deprecated for now
-        # actions_list = list(itertools.product([-1,1], repeat=len(self.active_joints)))
-        # self.actions_dict = {count: np.array(action) for count, action in enumerate(actions_list)}
-        # Set number of actions (number of joints to move)
-        # self.action_space.n = len(self.active_joints)
-
         # TODO make this dynamically update depending on # of joints
+        cartesian_pose_low = np.array([-1, -1, -1])
+        cartesian_pose_high = np.array([1, 1, 1])
+        joint_angles_low = np.array([-1]*len(self.active_joints))
+        joint_angles_high = np.array([1]*len(self.active_joints))
+
         self.observation_space = spaces.Box
-        self.observation_space.low  = np.array([0, 0, 0, 0, 0, 0, 0])
-        self.observation_space.high = np.array([1, 1, 1, 1, 1.5, 1.5, 1.5])
+        self.observation_space.low  = np.concatenate((joint_angles_low, cartesian_pose_low))
+        self.observation_space.high = np.concatenate((joint_angles_high, cartesian_pose_high))
 
         # Observations: position of joints, mapped to the full range of a joint. Last three vals in array are position
         self.observation_space = spaces.Box(low = self.observation_space.low, high=self.observation_space.high, dtype=np.float32) 
@@ -59,16 +76,16 @@ class ContinuousArmMotionEnvironment(gym.Env):
         self.hist = None
         self.hist_list = []
 
-        self.save_to_disk = save_to_disk
+        self.ep_count = 0
 
-        # self.fig = plt.figure()
+        self.save_to_disk = save_to_disk
 
         self.target_dict = {}
 
         if target_dict is None:
             self.num_targets = 5
             for i in range(0, self.num_targets):
-                self.target_dict.update({i: Point(random.uniform(0.5,0.8),random.uniform(0.5,0.8),random.uniform(0,0.8))})
+                self.target_dict.update({i: Point(random.uniform(self.box['x']['Min'],self.box['x']['Max']),random.uniform(self.box['y']['Min'],self.box['y']['Max']),random.uniform(self.box['z']['Min'],self.box['z']['Max']))})
         else:
             self.target_dict = target_dict
             self.num_targets = len(target_dict)
@@ -101,14 +118,15 @@ class ContinuousArmMotionEnvironment(gym.Env):
                     valid_pose = False
 
         return valid_pose
-
+    
     def step(self, action):
+        target_tolerance = 0.1
         # Case where we command robot to exceed its joint ranges. Establish high penalty
         self.action_count += 1
         if not self.valid_pose(self._action_to_angles(action)):
             reward = -5
             obs = self._next_observation()
-            done = self.S.distance_from_target(self.target_pos) < 0.01 or self.action_count == 50
+            done = self.S.distance_from_target(self.target_pos) < target_tolerance or self.action_count == 50
             self.prev_dist = self.S.distance_from_target(self.target_pos)
             return obs, reward, done, {}
         else:
@@ -127,7 +145,7 @@ class ContinuousArmMotionEnvironment(gym.Env):
 
             done = False
             
-            if self.S.distance_from_target(self.target_pos) < 0.1:
+            if self.S.distance_from_target(self.target_pos) < target_tolerance:
                 reward = 200
                 done = True
 
@@ -141,19 +159,25 @@ class ContinuousArmMotionEnvironment(gym.Env):
         return obs, reward, done, {}
 
     def _next_observation(self):
-        e = self.S.endpoint
+        e = self.target_pos
+
         obs = np.zeros(len(self.active_joints) + 3)
 
         i = 0
         for joint, angle in enumerate(self.S.angles):
             if joint in self.active_joints:
-                obs[i] = self.S.angles[joint]
+                ang_normalized = self.norm(angle, self.active_joints[joint]['Max'], self.active_joints[joint]['Min']) 
+                obs[i] = ang_normalized
                 i+=1
 
-        # obs[0:len(self.active_joints)] = np.array(self.S.angles)
-        obs[len(self.active_joints):10] = np.array([e.x, e.y, e.z])
+        obs[len(self.active_joints):10] = np.array([self.norm(e.x, self.box['x']['Max'], self.box['x']['Min']), 
+                                                    self.norm(e.y, self.box['y']['Max'], self.box['y']['Min']),
+                                                    self.norm(e.z, self.box['z']['Max'], self.box['z']['Min'])])
 
         return obs
+
+    def norm(self, x, x_max, x_min):
+        return 2 * (x-x_min)/(x_max-x_min) - 1
 
     def reset(self):
         # Reset the state of the environment to an initial state
@@ -186,24 +210,20 @@ class ContinuousArmMotionEnvironment(gym.Env):
 
         self.hist = EpisodeHistory(0, self.init_pos, self.target_pos)
 
+        self.ep_count +=1
+
+        # Save every 500 intervals
+        save_interval = 2
+        if self.ep_count % save_interval == 0:
+            pickle.dump(self, open(os.path.join(self.save_dir, "env_autosave.p"), "wb" ))
+
         return self._next_observation()
 
     def plot_rewards(self, added_title):
         reward_list = [x.total_reward() for x in self.hist_list]
         plt.plot(reward_list)
-        plt.title(f'Rewards vs time for {len(reward_list)} episodes and {added_title} learning rate')
+        plt.title(f'Rewards vs time for {len(reward_list)} episodes')
         plt.xlabel('Episode')
         plt.ylabel('Reward')
 
-        # x = range(reward_list)
-        # y = reward_list
-
-        # z = np.polyfit(x, y, 1)
-        # p = np.poly1d(z)
-        # plt.plot(x,p(x),"r--")
-
         plt.tight_layout()
-        plt.savefig(f'images/lr_experiment/lr{int(10e5*added_title)}.png')
-
-        plt.close()
-        # plt.show()
