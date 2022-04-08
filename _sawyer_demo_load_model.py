abdcf7afbe1655858f21f00b80966a833aa67175
Felipe Leguizamo <leguizamofelipe@outlook.com>
2022-04-07 23:49:29 -0500

    More robust reloading of model after Gazebo crash

--- _sawyer_demo_load_model.py
+++ _sawyer_demo_load_model.py
@@ -1,32 +1,57 @@
+from turtle import done
 from torch import device
 from sawyer_continuous_env import *
 from stable_baselines3 import PPO
 
-time_steps = 500000
-save_rate = 50000
+time_steps = 300000
+save_rate = 10000
 
-# USER DEFINED SECTION
-trained_agent = 'logs/n1_Points_PPO.zip'
+# ****************** USER DEFINED SECTION ***************
+trained_agent = 'logs/PPO-0.707-0.712-0.719-1649381651/model_after500000.zip'
 # ********************************************************
+
 # Load the trained agent
 model = PPO.load(trained_agent,  device = 'cuda')
-first_loop = True
-
 target_dict = { 0: Point(0.707,0.712,0.719)}
 
-env = ContinuousArmMotionEnvironment(sim_type='Gazebo', target_dict = target_dict, save_to_disk=True)
+# Environment initialization
+env_id = f'PPO-Gazebo-{target_dict[0].x}-{target_dict[0].y}-{target_dict[0].z}-{int(time.time())}'
 
+env = ContinuousArmMotionEnvironment(sim_type='Gazebo', target_dict = target_dict, save_to_disk=True, env_id = env_id)
+model.set_env(env)
+
+# Get baseline for how far off the trained model is
+obs = env.reset()
+
+done = False
+while not done:
+    action, _states = model.predict(obs)
+    obs, rewards, done, info = env.step(action)
+    init_distance = env.S.distance_from_target(target_dict[0])
+
+autosave_path = os.path.join('logs', env_id, 'Autosave')
+
+first_loop = True
 for i in range(1, int(time_steps/save_rate) + 1):
     if first_loop:
-        pass
+        first_loop = False
     else:
-        model = PPO.load("Autosave.zip",  device = 'cuda')
+        model = PPO.load(autosave_path,  device = 'cuda')
+        model.set_env(env)
 
-    model.set_env(env)
     try:
         model.learn(total_timesteps=int(save_rate), n_eval_episodes = 30)
-    except:
-        pass
+    except Exception as e:
+        print(e)
 
-    model.save('Autosave')
-    print(f'Autosaved at {len(env.hist_list)}')
+    model.save(autosave_path)
+    print(f'Autosaved at {len(env.hist_list)} episodes')
+
+done = False
+while not done:
+    action, _states = model.predict(obs)
+    obs, rewards, done, info = env.step(action)
+    final_distance = env.S.distance_from_target(target_dict[0])
+
+
+print(f'Init dist - {init_distance}, Final dist - {final_distance}')
