3c259e1c49eeecb5ad9659da5b917f13b3df0b83
Felipe Leguizamo <leguizamofelipe@outlook.com>
2022-04-09 15:37:03 -0500

    Even better depickler

--- depickler.py
+++ depickler.py
@@ -1,16 +1,36 @@
 import pickle
 import os
 import matplotlib.pyplot as plt
+import pandas as pd
 
 package_dir = 'packages'
+for name in ['env_autosave.p', 'env_hist_list_autosave.p']:
+    for package in os.listdir(package_dir):
+        
+        try:
+            pack_dir = os.path.join(package_dir, package)
+            env = pickle.load(open(os.path.join(pack_dir,name), 'rb'))
+            env.plot_rewards('')
+            plt.savefig(os.path.join(pack_dir,f'Rewards-{package}.png'))
+            plt.close()
+            env.hist_list[-1].plot_episode_3D()
+            plt.savefig(os.path.join(pack_dir,f'Last_Ep-{package}.png'))
+            plt.close()
+        except Exception as e:
+            pass
+        try:
+            pack_dir = os.path.join(package_dir, package)
+            env_hist_list = pickle.load(open(os.path.join(pack_dir,name), 'rb'))
+            env_hist_list[-1].plot_episode_3D()
+            plt.savefig(os.path.join(pack_dir,f'Last_Ep-{package}.png'))
+            plt.close()
+        except:
+            pass
+        try:
+            rewards = pd.read_csv(os.path.joint(pack_dir, 'rewards.csv'))
+            plt.plot(rewards['Reward'])
+            plt.savefig(os.path.join(pack_dir,f'Rewards-{package}.png'))
+            plt.close()
+        except Exception as e:
+            pass
 
-for package in os.listdir(package_dir):
-    try:
-        pack_dir = os.path.join(package_dir, package)
-        hist_list = pickle.load(open(os.path.join(pack_dir,'env_hist_list_autosave.p'), 'rb'))
-        hist_list[-3].plot_episode_3D()
-        plt.savefig(os.path.join(pack_dir,f'{package}.png'))
-        plt.close()
-    except Exception as e:
-        print(e)
-        print(f'Failed to load env for {package}')
