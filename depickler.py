929d4322ca12a5de6798d2667eb45643c0a0c05a
Felipe Leguizamo <leguizamofelipe@outlook.com>
2022-04-09 15:34:14 -0500

    Better depickler

--- depickler.py
+++ depickler.py
@@ -7,9 +7,10 @@ package_dir = 'packages'
 for package in os.listdir(package_dir):
     try:
         pack_dir = os.path.join(package_dir, package)
-        env = pickle.load(open(os.path.join(pack_dir,'env_autosave.p'), 'rb'))
-        env.plot_rewards('')
+        hist_list = pickle.load(open(os.path.join(pack_dir,'env_hist_list_autosave.p'), 'rb'))
+        hist_list[-3].plot_episode_3D()
         plt.savefig(os.path.join(pack_dir,f'{package}.png'))
+        plt.close()
     except Exception as e:
         print(e)
         print(f'Failed to load env for {package}')
