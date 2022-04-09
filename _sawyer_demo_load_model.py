d9341b2609b48a610cf0f4d0fcc561ffa3529b1a
Felipe Leguizamo <leguizamofelipe@outlook.com>
2022-04-09 12:52:24 -0500

    Better instrumentation for gazebo train

--- _sawyer_demo_load_model.py
+++ _sawyer_demo_load_model.py
@@ -2,57 +2,64 @@ from turtle import done
 from torch import device
 from sawyer_continuous_env import *
 from stable_baselines3 import PPO
+import shutil
 
 time_steps = 300000
 save_rate = 10000
 
 # ****************** USER DEFINED SECTION ***************
-trained_agent = 'logs/PPO-0.59-0.734-0.63-1649388342/model_after500000.zip'
+# trained_agent = ['logs/PPO-0.533-0.792-0.719-1649451317/model_after501760.zip', 'logs/PPO-0.766-0.525-0.781-1649453106/model_after501760.zip'] 
 # ********************************************************
 
-# Load the trained agent
-model = PPO.load(trained_agent,  device = 'cuda')
-target_dict = { 0: Point(0.590,0.734,0.630)}
+trained_agent = ['logs/PPO-0.766-0.525-0.781-1649453106/model_after501760.zip'] 
+target_dicts = [{ 0: Point(0.766,0.525,0.781)}]
 
-# Environment initialization
-env_id = f'PPO-Gazebo-{target_dict[0].x}-{target_dict[0].y}-{target_dict[0].z}-{int(time.time())}'
+for count, i in enumerate(target_dicts):
+    # Load the trained agent
+    model = PPO.load(trained_agent[count],  device = 'cuda')
+    
+    target_dict = target_dicts[count]
+    # Environment initialization
+    env_id = f'PPO-Gazebo-{target_dict[0].x}-{target_dict[0].y}-{target_dict[0].z}-{int(time.time())}'
 
-env = ContinuousArmMotionEnvironment(sim_type='Gazebo', target_dict = target_dict, save_to_disk=True, env_id = env_id)
-model.set_env(env)
+    env = ContinuousArmMotionEnvironment(sim_type='Gazebo', target_dict = target_dict, save_to_disk=True, env_id = env_id)
+    model.set_env(env)
 
-# Get baseline for how far off the trained model is
-obs = env.reset()
+    # Get baseline for how far off the trained model is
+    obs = env.reset()
 
-done = False
-while not done:
-    action, _states = model.predict(obs)
-    obs, rewards, done, info = env.step(action)
-    init_distance = env.S.distance_from_target(target_dict[0])
+    done = False
+    while not done:
+        action, _states = model.predict(obs)
+        obs, rewards, done, info = env.step(action)
+        init_distance = env.S.distance_from_target(target_dict[0])
 
-autosave_path = os.path.join('logs', env_id, 'Autosave')
+    autosave_path = os.path.join('logs', env_id, 'Autosave')
 
-first_loop = True
-for i in range(1, int(time_steps/save_rate) + 1):
-    if first_loop:
-        first_loop = False
-    else:
-        model = PPO.load(autosave_path,  device = 'cuda')
-        model.set_env(env)
+    first_loop = True
+    for i in range(1, int(time_steps/save_rate) + 1):
+        if first_loop:
+            first_loop = False
+        else:
+            model = PPO.load(autosave_path,  device = 'cuda')
+            model.set_env(env)
 
-    try:
-        model.learn(total_timesteps=int(save_rate), n_eval_episodes = 30)
-    except Exception as e:
-        print(e)
+        try:
+            model.learn(total_timesteps=int(save_rate), n_eval_episodes = 30)
+        except Exception as e:
+            print(e)
 
-    model.save(autosave_path)
-    print(f'Autosaved at {len(env.hist_list)} episodes')
+        model.save(autosave_path)
+        print(f'Autosaved at {len(env.hist_list)} episodes')
 
-done = False
-while not done:
-    action, _states = model.predict(obs)
-    obs, rewards, done, info = env.step(action)
-    final_distance = env.S.distance_from_target(target_dict[0])
+    done = False
+    while not done:
+        action, _states = model.predict(obs)
+        obs, rewards, done, info = env.step(action)
+        final_distance = env.S.distance_from_target(target_dict[0])
 
-print(f'Init dist - {init_distance}- Final dist - {final_distance}')
+    print(f'Init dist - {init_distance}- Final dist - {final_distance}')
 
-os.makedirs(os.path.join('logs', env_id, f'Init dist - {init_distance}- Final dist - {final_distance}'))
+    os.makedirs(os.path.join('logs', env_id, f'Init dist - {init_distance}- Final dist - {final_distance}'))
+
+    shutil.copyfile('logs/reward.csv', os.path.join('logs', env_id, 'rewards.csv'))
